de71f2d2a27bb26a028fda93bb902a0cb4676ab1
Srinivas <gadwalasrinivas559@gmail.com>
2025-06-24 19:27:52 +0530

    added end to end test demo file

---
+++ EndToEndTestDemo.py
@@ -0,0 +1,36 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+driver = webdriver.Chrome()  # chrome driver initiation
+driver.implicitly_wait(5)  # globally wait applied
+driver.maximize_window()
+driver.get("https://rahulshettyacademy.com/angularpractice/")  # testing rahul shetty practice page
+
+# Scenario - select a product from product list, checkout and purchase the product in the shop
+driver.find_element(By.CSS_SELECTOR, "a[href*='shop']").click()
+productToSelect = "Nokia Edge"
+productElements = driver.find_elements(By.CSS_SELECTOR, "div.card")
+for productElement in productElements:
+    productTitle = productElement.find_element(By.CSS_SELECTOR, ".card-title a").text
+    if productTitle == productToSelect:
+        productElement.find_element(By.CSS_SELECTOR, ".card-footer button").click()
+
+driver.find_element(By.CSS_SELECTOR, "a.btn-primary").click()
+sleep(2)  # To visually view the output
+driver.find_element(By.CSS_SELECTOR, "button.btn-success").click()
+countryToSelect = "India"
+driver.find_element(By.CSS_SELECTOR, "#country").send_keys(countryToSelect)
+wait = WebDriverWait(driver, 10)
+wait.until(expected_conditions.presence_of_element_located((By.LINK_TEXT, countryToSelect)))
+driver.find_element(By.LINK_TEXT, countryToSelect).click()
+driver.find_element(By.CSS_SELECTOR, "div.checkbox label").click()
+driver.find_element(By.CSS_SELECTOR, "input[value='Purchase']").click()
+alertMessage = driver.find_element(By.CSS_SELECTOR, "div.alert").text
+assert "Success!" in alertMessage
+
+sleep(2)  # To visually view the output
+driver.quit()
